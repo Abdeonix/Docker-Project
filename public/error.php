a515609bee00ce902280e1ddc736c94a5c0b4379
Mahmoud Shaaban <shabanmahmoud920@gmail.com>
2024-05-11 10:17:16 -0400

    project added

--- public/error.php
+++ public/error.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Error</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
@@ -12,20 +13,22 @@
     </style>
 </head>
 <body>
-    <div class="wrapper">
+    <div class="wrapper" role="main">
+        <header>
+            <h1>Invalid Request</h1>
+        </header>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>Invalid Request</h1>
-                    </div>
                     <div class="alert alert-danger fade in">
                         <p>Sorry, you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
                     </div>
                 </div>
             </div>        
         </div>
+        <footer>
+            <p>Contact us at support@FCDS.com for assistance.</p>
+        </footer>
     </div>
 </body>
 </html>
-
